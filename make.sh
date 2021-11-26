a3671588c887492cd7a65a6a6a1db349e06a38dd
ProkopHapala <ProkopHapala@gmail.com>
2021-11-26 18:09:38 +0100

    modifications for debugging FireCore read_2c.f90

--- make.sh
+++ make.sh
@@ -3,9 +3,11 @@
 set -e   # stop on error  ; see  https://stackoverflow.com/questions/3474526/stop-on-first-error
 
 dr=`pwd`
-python build_utils/gen_makefile.py
+cd build_utils
+python gen_makefile.py
+#python build_utils/gen_makefile.py
 #ln -s Makefile ../build/Makefile
-
+cd $dr
 
 mkdir build  || true
 cd    build
